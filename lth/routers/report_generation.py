b4c8e100643c96d0f73c89a45fc84f1266d183ef
oiehhun <xogns5037@naver.com>
2025-02-23 01:52:33 +0900

    code update

--- lth/routers/report_generation.py
+++ lth/routers/report_generation.py
@@ -4,8 +4,11 @@ from langchain_openai import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
 from langchain.prompts import PromptTemplate
+from langchain.schema.runnable import RunnableLambda
 from dotenv import load_dotenv
 from typing import List
+import warnings
+warnings.filterwarnings("ignore")
 
 
 router = APIRouter(prefix="/report", tags=["Report Generation"])
@@ -37,16 +40,19 @@ report_prompt = PromptTemplate(
         [권고 사항]
 
         [대응 방안]
-
+        
         [온라인 그루밍 피해 지원 기관 정보]
+        - 디지털성범죄피해자지원센터(02-734-8994 365일 상담가능) : 피해접수, 상담, 유포 현황 모니터링, 수사·법률·의료 연계 지원 
+        - 서울디지털성범죄 안심지원센터(02-815-0382/휴일, 야간 02-1366) : 상담지원, 수사지원, 법률지원, 심리상담지원, 의료 지원, 피해 촬영물 삭제 지원
+        - 경찰청 사이버수사국(긴급신고 112, 365일 24시간 상담): 사이버범죄 신고시스템(신고·상담·제보)
+        - 청소년사이버 상담센터(청소년전화 1388, 365일 24시간 상담): 온라인(채팅/게시판 등)·카카오톡 및 문자 상담
         """
     ),
     input_variables=["summary", "date", "chat_platform", "predators_name", "child_name"],
 )
 
-# 🔹 Pydantic 모델 (JSON 형식 정확히 맞춤)
 class ChatText(BaseModel):
-    text: List[str]  # ✅ 리스트 형식 맞음
+    text: List[str]
 
 class ReportRequest(BaseModel):
     chat_text: ChatText
@@ -63,12 +69,16 @@ async def generate_report(request: ReportRequest):
     summary_chain = load_summarize_chain(llm, chain_type="stuff", prompt=summary_prompt, document_variable_name="input")
     summary = summary_chain.run([document])
 
-    report = report_prompt.format(
+    generate = RunnableLambda(lambda summary: llm.invoke(report_prompt.format(
         summary=summary,
         date=request.date,
         chat_platform=request.chat_platform,
         predators_name=request.predators_name,
         child_name=request.child_name,
-    )
-
-    return {"report": report}
+    )))
+    
+    final_report = generate.invoke(summary)
+    
+    print("===============온라인 그루밍 의심 대화 리포트===============")
+    print("📝 보고서 내용:\n",final_report.content)
+    return {"report": final_report.content}
--- lth/routers/text_classification.py
+++ lth/routers/text_classification.py
@@ -26,14 +26,15 @@ skepticism = 3
 window_num = 5
 window_size = 10
 detection_list = deque(maxlen=window_num)
-chat_list = deque(maxlen=window_num - 1)
+chat_list = deque(maxlen=window_num)
 
 class ChatText(BaseModel):
     text: list[str]
 
 @router.post("/predict/")
 async def predict(chat_text: ChatText):
-    chat = preprocess(chat_text.text)
+    chat_text = [c.replace('\u200b', '') for c in chat_text.text]
+    chat = preprocess(chat_text)
     tokenized_input = tokenize_function(chat)
 
     inputs = tokenized_input["input_ids"].to("cuda")
@@ -44,14 +45,23 @@ async def predict(chat_text: ChatText):
     
     prediction = torch.argmax(output.logits, dim=-1).cpu().item()
     detection_list.append(prediction)
+    print("===============실시간 온라인 그루밍 탐지===============")
+    print('✅ 채팅 내용:', chat)
+    print('✅ 최근 윈도우 예측:', detection_list)
+    print('✅ 현재 채팅 예측:', prediction)
+    print()
 
     # 윈도우 크기를 초과할 경우 채팅 저장(최근 윈도우 개수-1 만큼)
-    if len(chat_text.text) == window_size:
-        chat_list.append(chat_text.text[0])
+    if len(chat_text) == window_size:
+        chat_list.append(chat_text[0])
 
     # 탐지 기준(skepticism) 초과 시
     if sum(detection_list) >= skepticism:
         # detection_list.clear()    # 탐지 시 deque 초기화
-        return {"prediction": 1, "chat_text": list(chat_list) + chat_text.text[1:]}
+        report_prompt = list(chat_list) + chat_text[1:]
+        print('🚨 푸시 알림 발송!')
+        print('✅ 탐지된 윈도우 채팅:', report_prompt)
+        print()
+        return {"prediction": 1, "chat_text": report_prompt}
     
-    return {"prediction": 0}
+    return {"prediction": 0, "chat_text": None}
